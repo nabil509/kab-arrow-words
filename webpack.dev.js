5836402241b68685e7d0d11d1671160e36910ec6
nabil509 <nabil509@gmail.com>
2020-09-22 22:51:57 +0200

    Update dependencies.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge'); // Merge config.
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   mode: 'development',
   devServer: {
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 3000,
     publicPath: '/assets/',
     hotOnly: true,
